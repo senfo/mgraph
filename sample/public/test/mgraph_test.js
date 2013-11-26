b74cee4e7f67e7ac4c9c3f84ecdfb9e2a3cd9f1c
Sean Fao <sean.fao@gmail.com>
2013-11-26 12:15:50 -0500

    Continued development of graph data model and unit tests.

--- sample/public/test/mgraph_test.js
+++ sample/public/test/mgraph_test.js
@@ -1,8 +1,55 @@
-describe('Array', function () {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      expect([1,2,3].indexOf(5)).toEqual(-1);
-      expect([1,2,3].indexOf(0)).toEqual(-1);
+describe('Graph', function () {
+    describe('Node', function () {
+        it('should allow fetching a node from the graph by its unique identifer', function () {
+            var expected = new MGraph.Node({
+                id: MGraph.id.generate(),
+                text: MGraph.id.generate(),
+                url: MGraph.id.generate()
+            });
+
+            MGraph.Graph.addNode(expected);
+
+            var actual = MGraph.Graph.getNodeById(expected.id);
+
+            expect(actual.id).toBe(expected.id);
+            expect(actual.text).toBe(expected.text);
+            expect(actual.url).toBe(expected.url);
+        });
+
+        it('should return the new node when created', function () {
+            var expectedId = MGraph.id.generate();
+            var expectedText = MGraph.id.generate();
+            var expectedUrl = MGraph.id.generate();
+            var actualNode = MGraph.Graph.createNode({
+                id: expectedId,
+                text: expectedText,
+                url: expectedUrl
+            });
+
+            expect(actualNode.id).toBe(expectedId);
+            expect(actualNode.text).toBe(expectedText);
+            expect(actualNode.url).toBe(expectedUrl);
+        });
+
+        it('should indicate that two nodes are adjacent', function () {
+            var node1 = new MGraph.Node({
+                id: MGraph.id.generate(),
+                text: 'node1',
+                url: null
+            });
+
+            var node2 = new MGraph.Node({
+                id: MGraph.id.generate(),
+                text: 'node2',
+                url: null
+            });
+
+            var edge = new MGraph.Edge({
+                id: MGraph.id.generate(),
+                edge: 'edge',
+                leftId: node1.id,
+                rightId: node2.id
+            });
+        });
     });
-  });
 });
--- src/mgraph.js
+++ src/mgraph.js
@@ -2,20 +2,56 @@
 var MGraph = (function (Kinetic) {
     "use strict";
 
-    var _nodes = [],
-        _edges = [],
+    var _nodes = {},
+        _edges = {},
         _adjacencies = {};
 
+    function _objectToArray(obj) {
+        var k, v, array = [];
+
+        for (k in obj) {
+            if (!obj.hasOwnProperty(k)) {
+                continue;
+            }
+
+            v = obj[k];
+            array.push(v);
+        }
+
+        return array;
+    }
+
+    function _addNode(node) {
+        _nodes[node.id] = node;
+    }
+
+    function _createNode(node) {
+    }
+
+    function _getNodeById(nodeId) {
+        return _nodes[nodeId];
+    }
+
     function _getNodes() {
-        return _nodes;
+        return _objectToArray(_nodes);
     }
 
     function _getEdges() {
-        return _edges;
+        return _objectToArray(_edges);
     }
 
     function _getAdjacencies() {
-        return _adjacencies;
+        return _objectToArray(_adjacencies);
+    }
+
+    function _s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    function _guid() {
+        return _s4() + _s4() + '-' + _s4() + '-' + _s4() + '-' + _s4() + '-' + _s4() + _s4() + _s4();
     }
 
     /**
@@ -68,11 +104,19 @@ var MGraph = (function (Kinetic) {
 
     return {
         /** @namespace */
-        graph: {
+        id: {
+            generate: _guid
+        },
+        /** @namespace */
+        Graph: {
+            addNode: _addNode,
+            createNode: _createNode,
+            getNodeById: _getNodeById,
             getNodes: _getNodes,
             getEdges: _getEdges,
             getAdjacencies: _getAdjacencies
         },
+        Edge: Edge,
         Node: Node
     };
 }(Kinetic));
